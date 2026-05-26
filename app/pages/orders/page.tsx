f46a8c020b561782e1921a5c99e262cf42a03cdd
UranzulPinecone <23LP3607@nest.edu.mn>
2026-05-26 17:25:28 +0800

    feat: disable status change when all transferred

--- app/pages/orders/page.tsx
+++ app/pages/orders/page.tsx
@@ -727,6 +727,7 @@ export default function OrdersPage() {
                             <Select
                               size="sm"
                               value={order.status}
+                              disabled={allTransferred}
                               onChange={(_, val) => {
                                 if (val)
                                   handleStatusChange(
@@ -738,6 +739,7 @@ export default function OrdersPage() {
                                 fontSize: "13px",
                                 fontWeight: 500,
                                 width: "100%",
+                                opacity: allTransferred ? 0.5 : 1,
                               }}
                             >
                               {STATUSES.map((s) => (
